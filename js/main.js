db6daa8684295aa8a4c10b2cd228f8f12da1ba29
Ekaterina <milady_95@bk.ru>
2020-06-08 19:27:46 +1000

    Отображение похожих объявлений на карте (ч. 3)

--- js/main.js
+++ js/main.js
@@ -1,55 +1,58 @@
 'use strict';
 
-var typeArray = ['palace', 'flat', 'house', 'bungalo'];
-var timeArray = ['12:00', '13:00', '14:00'];
-var featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var TYPE_ARRAY = ['palace', 'flat', 'house', 'bungalo'];
+var TIME_ARRAY = ['12:00', '13:00', '14:00'];
+var FEATURES_ARRAY = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var WIDTH_MAP = 947.5;
+var OFFSET_BY_X = 32.5;
+var OFFSET_BY_Y = 65;
 
 var getRandomInRange = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 var getRandomElement = function (arrayWithElements) {
-  var randomIndex = Math.floor(Math.random() * arrayWithElements.length);
+  var randomIndex = getRandomInRange(0, arrayWithElements.length - 1);
   return arrayWithElements[randomIndex];
 };
 
 var getArrayWithRandomElements = function (originalArray) {
   var newArray = [];
+  var arrayCopy = originalArray.slice();
 
-  for (var i = 0; i <= getRandomInRange(1, originalArray.length); i++) {
-    var randomIndex = Math.floor(Math.random() * originalArray.length);
-    var randomElement = originalArray[randomIndex];
+  for (var i = 0; i <= getRandomInRange(0, originalArray.length - 1); i++) {
+    var randomElement = getRandomElement(arrayCopy);
     newArray.push(randomElement);
-    // originalArray.splice(randomIndex, 1);
+    arrayCopy.splice(arrayCopy.indexOf(randomElement), 1);
   }
 
   return newArray;
 };
 
 var getOffer = function () {
-  var locationX = getRandomInRange(1, 1000);
-  var locationY = getRandomInRange(1, 1000);
+  var locationX = getRandomInRange(0, WIDTH_MAP);
+  var locationY = getRandomInRange(130, 630);
 
   var offerMap = {
     author: {
-      avatar: 'img/avatars/user0' + getRandomInRange(1, 8)
+      avatar: 'img/avatars/user0' + getRandomInRange(1, 8) + '.png'
     },
     offer: {
       title: 'Заголовок предложения',
       address: locationX + ', ' + locationY,
       price: '5000',
-      type: getRandomElement(typeArray),
+      type: getRandomElement(TYPE_ARRAY),
       rooms: getRandomInRange(1, 4),
       guests: getRandomInRange(1, 10),
-      checkin: getRandomElement(timeArray),
-      checkout: getRandomElement(timeArray),
-      features: getArrayWithRandomElements(featuresArray),
+      checkin: getRandomElement(TIME_ARRAY),
+      checkout: getRandomElement(TIME_ARRAY),
+      features: getArrayWithRandomElements(FEATURES_ARRAY),
       description: 'Описание предложения',
       photos: 'http://o0.github.io/assets/images/tokyo/hotel' + getRandomInRange(1, 3) + '.jpg'
     },
     location: {
-      x: getRandomInRange(1, 1000),
-      y: getRandomInRange(130, 630)
+      x: locationX,
+      y: locationY
     }
   };
 
@@ -66,7 +69,35 @@ var generateArray = function () {
   return array;
 };
 
-generateArray();
+var renderMapPin = function (pin) {
+  var mapPinElement = mapPin.cloneNode(true);
+
+  mapPinElement.style.left = pin.location.x + OFFSET_BY_X + 'px';
+  mapPinElement.style.top = pin.location.y - OFFSET_BY_Y + 'px';
+  mapPinElement.querySelector('img').src = pin.author.avatar;
+  mapPinElement.querySelector('img').alt = pin.offer.title;
+
+  return mapPinElement;
+};
 
 var mapBlock = document.querySelector('.map');
 mapBlock.classList.remove('map--faded');
+
+var mapPinsBlock = mapBlock.querySelector('.map__pins');
+
+var mapPin = document.querySelector('#pin')
+    .content
+    .querySelector('.map__pin');
+
+var createElements = function () {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < generateArray().length; i++) {
+    fragment.appendChild(renderMapPin(generateArray()[i]));
+  }
+
+  return fragment;
+};
+
+mapPinsBlock.appendChild(createElements());
+

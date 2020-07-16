eaee2737a6b7eb95a8ba5a8af2f4eaa6476b2634
Ekaterina <milady_95@bk.ru>
2020-07-16 11:48:57 +1000

    Перемещение метки по карте (5)

--- js/pin-moving.js
+++ js/pin-moving.js
@@ -6,27 +6,21 @@
   var addressInput = document.querySelector('#address');
 
   var MAP_LIMITS = {
-    minY: 130,
-    maxY: 630
-  };
-
-  var limits = {
-    top: map.offsetTop + MAP_LIMITS.minY,
-    right: map.offsetWidth - mapPin.offsetWidth,
-    bottom: MAP_LIMITS.maxY,
+    top: 130,
+    right: map.offsetLeft + map.offsetWidth - mapPin.offsetWidth,
+    bottom: 630,
     left: map.offsetLeft
   };
 
   var pinMovingListener = function () {
     mapPin.addEventListener('mousedown', function (evt) {
-      evt.preventDefault();
+      var coordsMax = document.documentElement.scrollWidth;
+      var coordsHeightMax = document.documentElement.scrollTop + document.documentElement.clientHeight - mapPin.offsetHeight;
+      var shiftX = evt.pageX - mapPin.offsetLeft;
+      var shiftY = evt.pageY - mapPin.offsetTop;
 
-      mapPin.style.zIndex = 9999;
-
-      var startCoords = {
-        x: evt.clientX,
-        y: evt.clientY
-      };
+      mapPin.style.position = 'absolute';
+      mapPin.style.zIndex = 1000;
 
       var onMouseMove = function (moveEvt) {
         moveEvt.preventDefault();
@@ -42,28 +36,38 @@
       };
 
       var movePin = function (moveEvt) {
-        startCoords = {
-          x: moveEvt.clientX,
-          y: moveEvt.clientY
-        };
+        var coordX = moveEvt.pageX - shiftX;
+        var coordY = moveEvt.pageY - shiftY;
 
-        if (startCoords.x > limits.right) {
-          var moveX = limits.right;
-          startCoords.x = moveX;
-        } else if (startCoords.x > limits.left) {
-          moveX = startCoords.x;
-          mapPin.style.left = moveX + 'px';
+        if (coordX < MAP_LIMITS.left) {
+          coordX = MAP_LIMITS.left;
         }
 
-        if (startCoords.y > limits.bottom) {
-          var moveY = limits.bottom;
-          startCoords.y = moveY;
-        } else if (startCoords.y > limits.top) {
-          moveY = startCoords.y;
-          mapPin.style.top = moveY + 'px';
+        if (coordX > MAP_LIMITS.right) {
+          coordX = MAP_LIMITS.right;
         }
 
-        addressInput.value = Math.round(moveX) + ', ' + Math.round(moveY);
+        if (coordX + mapPin.offsetWidth > coordsMax) {
+          coordX = coordsMax - mapPin.offsetWidth;
+        } else {
+          mapPin.style.left = coordX + 'px';
+        }
+
+        if (coordY < MAP_LIMITS.top) {
+          coordY = MAP_LIMITS.top;
+        }
+
+        if (coordY > MAP_LIMITS.bottom) {
+          coordY = MAP_LIMITS.bottom;
+        }
+
+        if (coordY > coordsHeightMax) {
+          document.documentElement.scrollTop += 20;
+        } else {
+          mapPin.style.top = coordY + 'px';
+        }
+
+        addressInput.value = Math.round(coordX) + ', ' + Math.round(coordY);
       };
 
       document.addEventListener('mousemove', onMouseMove);
